627f7b495b36c24a086386cde0597d8661622181
wangzhen5 <wangzhenplus@126.com>
2020-02-13 11:28:45 +0800

    第一次测试提交

---
+++ src/Demo.java
@@ -0,0 +1,26 @@
+import java.util.Date;
+
+public class Demo {
+    public static void main(String[] args){
+        System.out.println("Hello World!");
+        System.out.println("Hello World!");
+        System.out.println("Hello World!");
+        System.out.println("Hello World!");
+        System.out.println("Hello World!");
+        System.out.println("Hello World!");
+        System.out.println("Hello World!");
+        System.out.println("Hello World!");
+        System.out.println("Hello World!");
+        System.out.println("Hello World!");
+        System.out.println("Hello World!");
+        System.out.println("Hello World!");
+        System.out.println("Hello World!");
+        System.out.println("Hello World!");
+        System.out.println("Hello World!");
+        System.out.println("Hello World!");
+        System.out.println("Hello World!");
+        System.out.println("Hello World!");
+        Date temp = new Date();
+        System.out.println();
+    }
+}
---
+++ src/Roles.java
@@ -0,0 +1,19 @@
+/**
+ * @Author: wangzhen5
+ * @Date: 2020/2/11 0011
+ * @Version: 1.0
+ */
+public class Roles {
+    public static void main(String[] args){
+
+
+    }
+
+    /**
+     *
+     * @param a
+     */
+    public void ccc(String a){
+
+    }
+}
